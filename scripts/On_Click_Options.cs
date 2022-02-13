69fbcf5d0252ba28157fcc0a4790107139e8194c
RaffaeleMarchisio <71812750+RaffaeleMarchisio@users.noreply.github.com>
2022-02-13 09:57:12 +0100

    Update On_Click_Options.cs

--- scripts/On_Click_Options.cs
+++ scripts/On_Click_Options.cs
@@ -1,3 +1,4 @@
+//includo le librerie che mi servono
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,36 +8,36 @@ public class On_Click_Options : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1);  //carico la selezione dei livelli
     }
     public void main_menu()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);  //carico il menù principale
     }
     public void level1()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(2);  //carico il livello 1 
     }
     
     public void level2()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(3); //carico il livello 2
     }
     public void level3()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(4); //carico il livello 3
     }
     public void level4()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(5); //carico il livello 4
     }
     public void Exit()
     {
-        Application.Quit();
+        Application.Quit(); //esco dal gioco
     }
     public void Back_selection_close_socket()
     {
-        FindObjectOfType<UDP_reciver>().isGameOver=true;
-        SceneManager.LoadScene(1);
+        FindObjectOfType<UDP_reciver>().isGameOver=true;  //cerco un oggetto di tipo UDP_reciver e pongo isGameover a true per chiudere il socket UDP di openpose
+        SceneManager.LoadScene(1);  //carico la selezione dei livelli
     }
 }
